cbd9c4a63ed160d504b66a1872b4827dbabf82a2
Ciprian Dorin Craciun <ciprian@volution.ro>
2019-12-21 23:05:07 +0200

    [examples]  Update `google` example to use newly introduced `chain` technique.

--- examples/google.py
+++ examples/google.py
@@ -1,23 +1,29 @@
 
 
-import httt
+from httt import tests, chain
 
 
-_tests = httt.tests ("google")
-_tests.requests_shared.with_endpoint ("https:www3.l.google.com:443")
-_tests.responses_shared.has_header ("server", "gws") .has_header ("date") .has_header ("alt-svc")
+_tests = tests (
+		"google",
+		requests = chain.with_endpoint ("https:www3.l.google.com:443"),
+		responses = chain
+				.has_header ("server", "gws")
+				.has_header ("date")
+				.has_header ("alt-svc"),
+		debug = True,
+	)
 
 
 _tests.new (
 		"apex-to-www-redirect",
-		_tests.requests.with_host ("google.com"),
-		_tests.responses.redirect_to ("https://www.google.com/"),
+		requests = chain.with_host ("google.com"),
+		responses = chain.redirect_to ("http://www.google.com/"),
 	)
 
 _tests.new (
 		"www-get-/",
-		_tests.requests.with_host ("www.google.com"),
-		_tests.responses.expect_200 () .has_body (),
+		requests = chain.with_host ("www.google.com"),
+		responses = chain.expect_200 () .has_body (),
 	)
 
 
