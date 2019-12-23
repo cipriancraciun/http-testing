7869d63716dacdaa40ba57805f6ec53e4de940ef
Ciprian Dorin Craciun <ciprian@volution.ro>
2019-12-23 23:30:18 +0200

    [examples]  Minor update to `google` example, mainly add `dump` support.

--- examples/google.py
+++ examples/google.py
@@ -1,6 +1,7 @@
 
 
 from httt import tests, chain
+import sys
 
 
 _tests = tests (
@@ -10,14 +11,14 @@ _tests = tests (
 				.has_header ("server", "gws")
 				.has_header ("date")
 				.has_header ("alt-svc"),
-		debug = True,
+		debug = False,
 	)
 
 
 _tests.new (
 		"apex-to-www-redirect",
 		requests = chain.with_host ("google.com"),
-		responses = chain.redirect_to ("http://www.google.com/"),
+		responses = chain.redirect_to ("https://www.google.com/"),
 	)
 
 _tests.new (
@@ -26,6 +27,13 @@ _tests.new (
 		responses = chain.expect_200 () .has_body (),
 	)
 
+_tests.new (
+		"fail",
+		requests = chain.with_host ("example.com"),
+		responses = chain.expect_200 () .has_body (),
+	)
 
-_tests.execute ()
+
+_execution = _tests.execute ()
+_execution.dump (sys.stdout)
 
