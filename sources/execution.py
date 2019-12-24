279c61514a88290d4a20e21f59dd56a10ffa4854
Ciprian Dorin Craciun <ciprian@volution.ro>
2019-12-24 13:04:44 +0200

    [execution]  Add initial support for execution statistics.

--- sources/execution.py
+++ sources/execution.py
@@ -17,6 +17,8 @@ class Execution (object) :
 		self._transport = Transport (self._context)
 		self._transactions = list ()
 		self._transactions_by_handle = dict ()
+		self._statistics = ExecutionStatistics ()
+		self._statistics._aggregated = dict ()
 		self._debug = None
 	
 	
@@ -24,42 +26,60 @@ class Execution (object) :
 		self._transcript.cut ()
 		self._transcript.internal (0x91657e21, "executing...")
 		_debug = self._debug if _debug is None else _debug
-		self._execute (_test, (), self._debug)
-		if len (self._transactions) == 0 :
-			self._transcript.info (0xc5327e15, "execution yielded no transactions!")
+		self._execute (_test, None, None, self._debug)
 		self._transcript.internal (0x310eac5e, "executed;")
 		self._transcript.cut ()
+		self._report_execution (_debug)
 	
 	
-	def _execute (self, _test, _stack, _debug) :
+	def _execute (self, _test, _tests_stack, _identifier_stack, _debug) :
 		if isinstance (_test, tests.Test) :
-			self._execute_test (_test, _stack, _debug)
+			self._execute_test (_test, _tests_stack, _identifier_stack, _debug)
 		elif isinstance (_test, tests.Tests) :
-			self._execute_tests (_test, _stack, _debug)
+			self._execute_tests (_test, _tests_stack, _identifier_stack, _debug)
 		else :
 			raise Exception (0xbe83caa9)
+		self._report_progress (_debug)
 	
 	
-	def _execute_tests (self, _tests, _stack, _debug) :
+	def _execute_tests (self, _tests, _tests_stack, _identifier_stack, _debug) :
 		
-		_stack = (_stack, _tests.identifier)
-		_identifier = stringify_identifier (_stack)
-		_handle = fingerprint (_stack)
+		_tests_stack = (_tests_stack, _tests)
+		_identifier_stack = (_identifier_stack, _tests.identifier)
+		_identifier = stringify_identifier (_identifier_stack)
+		_handle = fingerprint (_identifier_stack)
 		
 		_debug = _tests._debug if _debug is None else _debug
 		self._transcript.debug (0xe605026e, "beginning [%s] `%s`...", _handle, _identifier)
 		
+		if _tests_stack[0] is None :
+			_statistics = self._statistics
+		else :
+			_statistics = _tests_stack[0][1]._statistics
+		if _tests.identifier in _statistics._aggregated :
+			_statistics = _statistics._aggregated[_tests.identifier]
+		else :
+			_statistics._aggregated[_tests.identifier] = ExecutionStatistics ()
+			_statistics = _statistics._aggregated[_tests.identifier]
+			_statistics._identifier = _tests.identifier
+			_statistics._aggregated = dict ()
+		
+		_tests._statistics = _statistics
+		
 		for _test in _tests._tests :
-			self._execute (_test, _stack, _debug)
+			self._execute (_test, _tests_stack, _identifier_stack, _debug)
 		
-		self._transcript.internal (0xd6ef2184, "finished [%s];", _handle)
+		if _tests._statistics.succeeded :
+			self._transcript.internal (0xd6ef2184, "finished [%s] `%s`;", _handle, _identifier)
+		else :
+			self._transcript.notice (0xd5081953, "finished [%s] `%s`:  %d executed;  %d (%.0f%%) failed;", _handle, _identifier, _tests._statistics.count_executed, _tests._statistics.count_failed, _tests._statistics.ratio_failed * 100)
 	
 	
-	def _execute_test (self, _test, _stack, _debug) :
+	def _execute_test (self, _test, _tests_stack, _identifier_stack, _debug) :
 		
-		_stack = (_stack, _test.identifier)
-		_identifier = stringify_identifier (_stack)
-		_handle = fingerprint (_stack)
+		_identifier_stack = (_identifier_stack, _test.identifier)
+		_identifier = stringify_identifier (_identifier_stack)
+		_handle = fingerprint (_identifier_stack)
 		
 		if _handle in self._transactions_by_handle :
 			self._transcript.error (0x0472ccf3, "duplicate [%s] `%s`;  ignoring!", _handle, _identifier)
@@ -75,7 +95,7 @@ class Execution (object) :
 		_transaction.execute ()
 		_succeeded = _transaction.enforce ()
 		
-		self._transactions.append ((_handle, _identifier, _transaction, _succeeded))
+		self._transactions.append ((_handle, _identifier, _transaction))
 		self._transactions_by_handle[_handle] = _transaction
 		
 		if _succeeded :
@@ -100,11 +120,98 @@ class Execution (object) :
 		
 		self._transcript.internal (0x4b83e138, "executed [%s];", _handle)
 		self._transcript.cut ()
+		
+		self._update_statistics (_tests_stack, _transaction)
+	
+	
+	def _update_statistics (self, _tests_stack, _transaction) :
+		
+		while _tests_stack is not None :
+			_tests_stack[1]._statistics._update (_transaction)
+			_tests_stack = _tests_stack[0]
+		
+		self._statistics._update (_transaction)
+	
+	
+	def _report_progress (self, _debug) :
+		
+		if not _debug and (self._statistics.count_executed % 10) != 0 or self._statistics.count_executed == 0 :
+			return
+		
+		self._transcript.cut ()
+		self._transcript.notice (0xe8e97c33, "execution progress:  %d executed;  %d (%.0f%%) failed;", self._statistics.count_executed, self._statistics.count_failed, self._statistics.ratio_failed * 100)
+		self._transcript.cut ()
+	
+	
+	def _report_execution (self, _debug) :
+		self._transcript.cut ()
+		
+		if len (self._transactions) > 0 :
+			if self._statistics.succeeded :
+				self._transcript.notice (0xd23fd5de, "execution outcome:  %d executed;  all succeeded;", self._statistics.count_executed)
+			else :
+				self._transcript.warning (0xe8e97c33, "execution outcome:  %d executed;  %d (%.0f%%) failed;", self._statistics.count_executed, self._statistics.count_failed, self._statistics.ratio_failed * 100)
+		else :
+			self._transcript.warning (0xc5327e15, "execution yielded no transactions!")
+		
+		def _report_statistics (_statistics, _transcript, _only_aggregated = False) :
+			
+			if not _only_aggregated :
+				_identifier = stringify_identifier (_statistics._identifier)
+				if _statistics.succeeded :
+					_transcript.info (0x9742017c, "`%s`:  %d executed;  all succeeded;", _identifier, _statistics.count_executed)
+				else :
+					_transcript.info (0x6c5fcc49, "`%s`:  %d executed;  %d (%.0f%%) failed;", _identifier, _statistics.count_executed, _statistics.count_failed, _statistics.ratio_failed * 100)
+				_transcript = _transcript.fork ()
+			
+			for _identifier, _statistics in sorted (_statistics._aggregated.iteritems ()) :
+				if _statistics.count_executed > 0 :
+					_report_statistics (_statistics, _transcript.fork (False))
+		
+		_report_statistics (self._statistics, self._transcript.fork (), True)
+		
+		self._transcript.cut ()
 	
 	
 	def _trace (self, _tracer) :
+		
 		if len (self._transactions) > 0 :
-			for _identifier, _handle, _transaction, _succeeded in sorted (self._transactions) :
+			
+			
+			_tracer_meta = _tracer.fork (False)
+			_tracer_meta.cut ()
+			_tracer_meta (0x00de4d05, "## statistics:")
+			_tracer_meta.indent ()
+			_tracer_meta (0x5cfb4baa, "executed total: %d;", self._statistics.count_executed)
+			_tracer_meta (0x5c414e7e, "executed succeeded: %d (%.0f%%);", self._statistics.count_succeeded, self._statistics.ratio_succeeded * 100)
+			_tracer_meta (0x3fc6ecdc, "executed failed: %d (%.0f%%);", self._statistics.count_failed, self._statistics.ratio_failed * 100)
+			_tracer_meta.cut ()
+			
+			
+			_tracer_meta = _tracer.fork (False)
+			_tracer_meta.cut ()
+			_tracer_meta (0x84cb7e16, "## tests:")
+			
+			def _report_statistics (_statistics, _tracer, _only_aggregated = False) :
+				
+				if not _only_aggregated :
+					_identifier = stringify_identifier (_statistics._identifier)
+					if _statistics.succeeded :
+						_tracer (0xcbac6bd3, "`%s`:  %d executed;  all succeeded;", _identifier, _statistics.count_executed)
+					else :
+						_tracer (0xa1fc227b, "`%s`:  %d executed;  %d (%.0f%%) failed;", _identifier, _statistics.count_executed, _statistics.count_failed, _statistics.ratio_failed * 100)
+					_tracer = _tracer.fork ()
+				
+				for _identifier, _statistics in sorted (_statistics._aggregated.iteritems ()) :
+					if _statistics.count_executed > 0 :
+						_report_statistics (_statistics, _tracer.fork (False))
+			
+			_report_statistics (self._statistics, _tracer_meta.fork (), True)
+			_tracer_meta.cut ()
+			
+			
+			for _identifier, _handle, _transaction in sorted (self._transactions) :
+				_succeeded = _transaction.succeeded
 				_tracer.cut ()
 				if not _succeeded :
 					_tracer (0xb86f3e97, "!!!! FAILED !!!!")
@@ -124,6 +231,7 @@ class Execution (object) :
 				if not _succeeded :
 					_tracer (0x14aaf57d, "!!!! FAILED !!!!")
 				_tracer.cut ()
+			
 		else :
 			_tracer (0x1cd6ab66, "## transactions: none;")
 	
@@ -131,4 +239,31 @@ class Execution (object) :
 	def dump (self, _stream = None) :
 		_tracer = dumper (_stream)
 		self._trace (_tracer)
+
+
+
+
+class ExecutionStatistics (object) :
+	
+	
+	def __init__ (self) :
+		
+		self.succeeded = True
+		
+		self.count_executed = 0
+		self.count_succeeded = 0
+		self.count_failed = 0
+	
+	
+	def _update (self, _transaction) :
+		
+		self.count_executed += 1
+		if _transaction.succeeded :
+			self.count_succeeded += 1
+		else :
+			self.count_failed += 1
+			self.succeeded = False
+		
+		self.ratio_succeeded = float (self.count_succeeded) / self.count_executed
+		self.ratio_failed = float (self.count_failed) / self.count_executed
 
